1d262cf0c45e2cb58e8505d8cac1bc024d5a30e1
will <devpedrowill@gmail.com>
2026-08-28 21:36:13 -0300

    modificacao .item

--- 28.08.2026/descricao_pedidos.py
+++ 28.08.2026/descricao_pedidos.py
@@ -1,4 +1,7 @@
 def descrever_pedidos (cliente, **extras):
     print ("Cliente: ", cliente)
-    for chave, valor in extras.items: 
+    for chave, valor in extras.items(): 
         print (f"{chave} - {valor}")
+
+dados = descrever_pedidos("Ana", observacao = "Sem cebola", retirada = False)
+
